ac9b528c46f55ef26183d00fedb39152f73d38da
Jay Nkuna <jaylancerdev@gmail.com>
2024-11-26 18:46:39 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,13 @@ use Illuminate\Support\Facades\Route;
 
 use App\Livewire\Customers;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 Route::get('/', function () {
-    return view('auth.login'); // Redirects to login
-})->middleware('guest');
+    return view('welcome');
+});
+// Route::get('/', function () {
+//     return view('auth.login'); // Redirects to login
+// })->middleware('guest');
+
 
 // Protect routes with the auth middleware
 // Route::middleware(['auth'])->group(function () {
@@ -21,3 +22,5 @@ Route::get('/customers', Customers::class);
 Route::get('/phpmyadmin', function () {
     abort(404);
 });
+
+require __DIR__.'/auth.php';
