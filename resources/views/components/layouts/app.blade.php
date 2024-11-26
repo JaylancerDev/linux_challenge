1f52d3f853a22f2af745f18abcfec0a2c52fbaca
Jay Nkuna <jaylancerdev@gmail.com>
2024-11-26 15:08:27 +0200

    Update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Livewire App</title>
+    <title>Linux Challenge | LAMP Server</title>
     <link href="https://cdn.jsdelivr.net/npm/toastr@2.1.4/build/toastr.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/toastr@2.1.4/build/toastr.min.js"></script>
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
@@ -15,21 +15,21 @@
     {{ $slot }}
     @livewireScripts
     <script>
-    document.addEventListener('livewire:load', function () {
-        Livewire.on('toast', (message, type) => {
-            if (['success', 'info', 'warning', 'error'].includes(type)) {
-                toastr.options = {
-                    "closeButton": true,
-                    "progressBar": true,
-                    "positionClass": "toast-top-right", // Adjust position as needed
-                    "timeOut": "5000", // Adjust display time
-                };
-                toastr[type](message);
-            } else {
-                console.error('Invalid Toastr type:', type);
-            }
-        });
-    });
+    // document.addEventListener('livewire:load', function () {
+    //     Livewire.on('toast', (message, type) => {
+    //         if (['success', 'info', 'warning', 'error'].includes(type)) {
+    //             toastr.options = {
+    //                 "closeButton": true,
+    //                 "progressBar": true,
+    //                 "positionClass": "toast-top-right", // Adjust position as needed
+    //                 "timeOut": "5000", // Adjust display time
+    //             };
+    //             toastr[type](message);
+    //         } else {
+    //             console.error('Invalid Toastr type:', type);
+    //         }
+    //     });
+    // });
 </script>
 </body>
 </html>
